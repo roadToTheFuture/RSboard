adcdf569705a7814682c9274f619ebeecef1c0b9
Mykola Nazimkov <nazimkoff@gmail.com>
2021-01-12 21:55:08 +0200

    init commit

--- src/js/components/board/board.js
+++ src/js/components/board/board.js
@@ -1,1 +1,7 @@
+import '@js/components/board/boardTitle/boardTitle.js';
 import './board.scss';
+import createDomElement from '@js/utils/createDomElement.js';
+
+const boardWrapper = createDomElement('div', 'board__wrapper', '', document.body);
+const titleWrapper = createDomElement('div', 'board__title-wrapper', '', boardWrapper);
+export { boardWrapper, titleWrapper };
---
+++ src/js/components/board/boardTitle/boardTitle.js
@@ -0,0 +1,3 @@
+import { titleWrapper } from '@js/board/board.js';
+import createDomElement from '@js/utils/createDomElement.js';
+createDomElement('h2', 'board__title', 'sboard', titleWrapper);
