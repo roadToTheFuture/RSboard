24ca34b06cbe31cc1a0da2fe2321fb9bbf3f9897
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-01-12 21:48:25 +0300

    fix: update structure

--- src/js/components/header/header.js
+++
@@ -1,4 +0,0 @@
-import { createDomElement } from '@js/utils/createDomElement.js';
-import { header } from '@js/components/body/body.js';
-
-createDomElement('h1', 'title', 'Covid dashboard', header);
