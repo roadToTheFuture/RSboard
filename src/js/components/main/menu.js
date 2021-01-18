ca9b13432b4308f373f677a276932718f468995a
Gleb Pobezhimov <pglb@yandex.ru>
2021-01-19 01:21:44 +0400

    feat: add clock to header;

--- src/js/components/main/menu.js
+++ src/js/components/main/menu.js
@@ -11,4 +11,36 @@ function menu() {
 const burger = document.querySelector('.burger');
     
 burger.addEventListener('click', menu)
+
+function showTime() {
+  let today = new Date(),
+  hour = today.getHours(),
+  min = today.getMinutes(),
+  sec = today.getSeconds();
+
+  time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+  setTimeout(showTime, 1000);
+}
+
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+function showDate() {
+  let today = new Date(),
+   day = today.getDay(),
+   dat = today.getDate(),
+   mon = today.getMonth();
+
+   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   let monthes = ["January", "February", "March", "April", "May", "June", "July", "August", "Septeember", "October", "Noember", "December"] 
+
+  date.innerHTML = `${days[day]}<span>, </span>${dat}<span>th </span>${monthes[mon]}`;
+
+  setTimeout(showDate, 1000);
+}
+
+showTime()
+showDate()
   
