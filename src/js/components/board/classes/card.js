49f29ad83fbc462621df1503bdc93719f6348061
Mykola Nazimkov <nazimkoff@gmail.com>
2021-01-24 20:05:10 +0200

    feat: add MUI library

--- src/js/components/board/classes/card.js
+++ src/js/components/board/classes/card.js
@@ -8,14 +8,16 @@ class Card {
 
   render() {
     const card = createDomElement('div', 'column__card', '', this.elem);
+
     const textarea = createDomElement('textarea', 'column__card__textarea', '', card);
     textarea.placeholder = this.defaultText;
+
     const buttonCardDelete = createDomElement('img', 'column__card__button', '', card);
     buttonCardDelete.src = 'https://user-images.githubusercontent.com/61156194/104500143-b310e500-55e6-11eb-900c-2c23db449231.png';
 
     buttonCardDelete.addEventListener('click', () => {
       card.remove();
     });
-}
+  }
 }
 export { Card };
